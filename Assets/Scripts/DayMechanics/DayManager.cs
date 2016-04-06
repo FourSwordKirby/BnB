e042cbeaa00a974f0f4ede51d493379bcd87013a
Francisco Rojo <frojo@andrew.cmu.edu>
2016-04-06 12:20:01 -0400

    restructuring/some outlining of Day/dinner

--- Assets/Scripts/DayMechanics/DayManager.cs
+++ Assets/Scripts/DayMechanics/DayManager.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class DayManager : MonoBehaviour {
 
+	public GameManager gameManager;
+	public DialogController dialogController;
+
+	public Room bedroom;
+	public Room dining;
+
+	private int remainingConvoPts;
+	private int dayNumber;
+
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -23,18 +34,51 @@ public class DayManager : MonoBehaviour {
 	 * Unlimited light conversations
 	 * Deep conversations where you talk about the person they're talking to 
 	 * 
+	 * After using 2 convo points: beau appears and says it's time for dinner
+	 * Choose a suitor to go to dinner with
+	 * Dinner is a stressful/emotional affair
 	 * 
+	 * After dinner, Beau appears and you go to bed
 	 * 
 	 */
 
 	void BeginDay() {
 
-		//
+		/* Housekeeping for keeping track of info */
+		remainingConvoPts = 2;
+		dayNumber++;
+
+		gameManager.LoadRoom (bedroom);
+
+		// dialogController.StartConversation (MorningBeau);
+
+
+
+	}
+
+	void DinnerEvent() {
+
+		// TODO: Choose suitor
+
+		// S
+
+		gameManager.LoadRoom (dining);
+
+		//dialogController.StartConversation (Dinner);
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (remainingConvoPts < 0) {
+			Debug.Log ("ERROR: Remaining conversation points dipped below 0!");
+		}
+
+		if (remainingConvoPts == 0) {
+			// TODO: Dinner time!
+		}
 	
 	}
 }
