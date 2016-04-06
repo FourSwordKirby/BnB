5a00ed5c97c1f056aea188382c65cfe30b01009f
Roger Liu <rogliu26@gmail.com>
2016-04-06 11:56:31 -0400

    removed unused scripts. Also got work started on suitor icons

---
+++ Assets/LoveInterestSelectController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoveInterestSelectController : MonoBehaviour {
+    public GameManager gameManager;
+    public List<LoveInterestIcon> loveInterestIcons;
+
+    public enum RoomPosition
+    {
+        FarLeft,
+        NearLeft,
+        Center,
+        NearRight,
+        FarRight
+    }
+
+    public void showLoveInterest(LoveInterest loveInterest, RoomPosition position)
+    {
+        loveInterestIcons[(int)position].LoadLoveInterest(loveInterest, LoveInterestClicked);
+    }
+
+    public void displayLoveInterests()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void hideLoveInterests()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void LoveInterestClicked(LoveInterest loveInterest)
+    {
+        Debug.Log(loveInterest);
+    }
+}
