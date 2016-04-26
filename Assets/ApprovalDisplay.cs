2d33ed61b6f5369f4437cf590d4af69dff6ca517
Francisco Rojo <frojo@andrew.cmu.edu>
2016-04-26 11:15:20 -0400

    some more work on diary

--- Assets/ApprovalDisplay.cs
+++ Assets/ApprovalDisplay.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ApprovalDisplay : MonoBehaviour {
 
 	public LoveInterest loveInterestInfo;
+	public List<Image> hearts;
 
 	// Use this for initialization
 	void Start () {
@@ -12,12 +15,25 @@ public class ApprovalDisplay : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	void ShowHearts(int numHearts) {
+
+
 	}
 
 	public void RefreshHeartDisplay() {
 		Debug.Log ("Refreshing hearts for " + loveInterestInfo.designation);
 		int approval = loveInterestInfo.approvalRaiting;
+
+		int numHearts;
+		if (approval <= 0)
+			numHearts = 0;
+		else
+			numHearts = ((approval - 1) / 25) + 1;
+
+		ShowHearts (numHearts);
 
 		// TODO: Logic for determining how many hearts to show
 	}
