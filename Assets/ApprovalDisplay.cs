b8341834699e9978b0e37127b2a8fa2133dbdef8
Francisco Rojo <frojo@andrew.cmu.edu>
2016-04-28 21:30:43 -0400

    marriage twees imported and gift in UI

--- Assets/ApprovalDisplay.cs
+++ Assets/ApprovalDisplay.cs
@@ -7,7 +7,7 @@ public class ApprovalDisplay : MonoBehaviour {
 
 	public LoveInterest loveInterestInfo;
 	public List<Image> hearts;
-
+	public Image giftIcon;
 	static int maxHearts = 4;
 	static int approvalPerHeart = 100 / maxHearts;
 
@@ -28,6 +28,19 @@ public class ApprovalDisplay : MonoBehaviour {
             else
                 hearts[i].color = new Color(hearts[i].color.r, hearts[i].color.g, hearts[i].color.b, 0.1f);
 		}
+		// 1 and 2 are greyed out and 3 is fully displaying
+
+	}
+
+	void ShowGift(int giftKey) {
+		// 1 and 2 are greyed out and 3 is fully displaying
+		if (giftKey == 1 || giftKey == 2)
+			giftIcon.color = new Color (giftIcon.color.r, giftIcon.color.g, giftIcon.color.b, 0.1f);
+		else if (giftKey >= 3)
+			giftIcon.color = new Color (giftIcon.color.r, giftIcon.color.g, giftIcon.color.b, 1f);
+		else
+			giftIcon.color = new Color (giftIcon.color.r, giftIcon.color.g, giftIcon.color.b, 0f);
+
 	}
 
 	public void RefreshHeartDisplay() {
@@ -41,6 +54,7 @@ public class ApprovalDisplay : MonoBehaviour {
 			numHearts = ((approval - 1) / approvalPerHeart) + 1;
 
 		ShowHearts (numHearts);
+		ShowGift (loveInterestInfo.giftStatus);
 
 		// TODO: Logic for determining how many hearts to show
 	}
